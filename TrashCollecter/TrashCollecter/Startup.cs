4a3c005d9ea5350f99c6305d969bfca8d86ea993
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-26 10:39:30 -0500

    Initial commit

---
+++ TrashCollecter/TrashCollecter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollecter.Startup))]
+namespace TrashCollecter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

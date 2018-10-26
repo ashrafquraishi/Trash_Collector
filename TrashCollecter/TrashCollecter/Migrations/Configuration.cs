f299e293ad960f1577d1b1b677acdf7ef9ff19b1
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-26 11:42:08 -0500

    Added Employee and Customer model class

---
+++ TrashCollecter/TrashCollecter/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+namespace TrashCollecter.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TrashCollecter.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TrashCollecter.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+            context.Roles.AddOrUpdate(r => r.Name,
+                new Microsoft.AspNet.Identity.EntityFramework.IdentityRole {Name = "Employee" },
+                new Microsoft.AspNet.Identity.EntityFramework.IdentityRole { Name = "Customer" }
+
+                );
+        }
+    }
+}

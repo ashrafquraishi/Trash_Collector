750b46f5f530ea68526265a418dd9bcf615e095b
ashraf quraishi <ashrafquraishi4@gmail.com>
2018-10-29 10:48:11 -0500

    Updated Employee and Customer Models

--- TrashCollecter/TrashCollecter/Models/CustomerModels.cs
+++ TrashCollecter/TrashCollecter/Models/CustomerModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,23 @@ namespace TrashCollecter.Models
 {
     public class CustomerModels
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Date Of Birth ")]
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        [Display(Name = "Zip Code")]
+
+        public int ZipCode { get; set; }
+
+       
+        
+
+
+
     }
 }
